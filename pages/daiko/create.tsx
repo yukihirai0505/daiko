4523e61d61e4e5f065a6ff713a371d7ff8177769
Yuki Hirai <yukihirai0505@gmail.com>
2019-10-01 11:24:42 +0900

    Add loading

--- pages/daiko/create.tsx
+++ pages/daiko/create.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { Fragment, useState } from 'react'
 import Routes from '../../src/constants/routes'
 import Router from 'next/router'
 import { AppWithAuthorization } from '../../src/components/Layout'
@@ -6,10 +6,10 @@ import { connect } from 'react-redux'
 import { createNewDaiko } from '../../src/utils/api'
 import Link from 'next/link'
 
-const create = async (e, authUser) => {
+const create = async (e, authUser, setLoading) => {
   e.preventDefault()
   // show loading
-
+  setLoading(true)
   const idToken = await authUser.getIdToken()
   const form: any = document.getElementById('new-daiko-form')
   const title = form.title.value
@@ -23,6 +23,7 @@ const create = async (e, authUser) => {
     })
     Router.push(Routes.DAIKO_DETAIL(id))
   }
+  setLoading(false)
 }
 
 const formFields = [
@@ -60,14 +61,33 @@ const formFields = [
 ]
 
 const Create = ({ authUser }) => {
+  const [isLoading, setLoading] = useState(false)
   return (
     <AppWithAuthorization>
+      {isLoading && (
+        <Fragment>
+          <link
+            rel="stylesheet"
+            href="https://pagecdn.io/lib/font-awesome/5.10.0-11/css/all.min.css"
+            integrity="sha256-p9TTWD+813MlLaxMXMbTA7wN/ArzGyW/L7c5+KkjOkM="
+            crossOrigin="anonymous"
+          />
+          <div className="w-full h-full fixed block top-0 left-0 bg-white opacity-75 z-50">
+            <span
+              className="text-green-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0"
+              style={{ top: '50%' }}
+            >
+              <i className="fas fa-circle-notch fa-spin fa-5x" />
+            </span>
+          </div>
+        </Fragment>
+      )}
       <div className="container mx-auto h-full flex justify-center items-center mt-8">
         <div className="w-full max-w-xl">
           <form
             id="new-daiko-form"
             className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-            onSubmit={e => create(e, authUser)}
+            onSubmit={e => create(e, authUser, setLoading)}
           >
             {formFields.map((field, key) =>
               field.isText ? (
@@ -76,7 +96,8 @@ const Create = ({ authUser }) => {
                     className="block text-gray-700 text-sm font-bold mb-2"
                     htmlFor={field.id}
                   >
-                    {field.text}{field.required ? '(必須)' : '(任意)'}{' '}
+                    {field.text}
+                    {field.required ? '(必須)' : '(任意)'}{' '}
                     <span className="text-sm text-gray-400">
                       * {field.maxLength}文字以下
                     </span>
@@ -99,7 +120,8 @@ const Create = ({ authUser }) => {
                     className="block text-gray-700 text-sm font-bold mb-2"
                     htmlFor={field.id}
                   >
-                    {field.text}{field.required ? '(必須)' : '(任意)'}{' '}
+                    {field.text}
+                    {field.required ? '(必須)' : '(任意)'}{' '}
                     <span className="text-sm text-gray-400">
                       * {field.maxLength}文字以下
                     </span>
